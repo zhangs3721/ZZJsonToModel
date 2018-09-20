07cca8a7bc8146128c7769b29fcee28fb55bd828
zhang <461818526@qq.com>
2018-09-20 12:28:52 +0800

    优化

--- ZZJsonToModel/ZZJsonToModel/ZZJsonToModel.h
+++ ZZJsonToModel/ZZJsonToModel/ZZJsonToModel.h
@@ -1,3 +1,9 @@
+
+// ZZJsonToModel(GitHub:https://github.com/zhangs3721/ZZJsonToModel )
+// 纯代码编写，无需安装任何插件，一个方法轻松搞定复杂Json转Model。
+// 如果 ZZJsonToModel 为您节约了时间，您的**🌟星星**是我优化项目的动力，谢谢🙏🙏🙏
+// 如果您发现了bug，或有新的想法和建议，请及时通知我qq（461818526/13146615588）。
+
 #import <Foundation/Foundation.h>
 
 static NSString *const kkPropertyTypeString = @"kkZZMonsterNSString";
